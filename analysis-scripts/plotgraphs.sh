a0223dbc116b29c61b8faf42e0fdd24f3d627ac8
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2017-12-06 14:23:16 +0100

    add missing parseData script, new plotgraphs script that we used for plotting

--- analysis-scripts/plotgraphs.sh
+++ analysis-scripts/plotgraphs.sh
@@ -1,5 +1,5 @@
 #plot and output crash statistics
-python3 crashStats.py --infolder ../SinusoidalJammingDetail/ --plotfolder graphs --plotname 'SinusoidalJammingDetail' --csvout SinusoidalJammingDetail.csv --plotDeltaV
-python3 injectionCrashStats.py --infolder ../SinusoidalPosInjection/ --plotfolder graphs --plotname 'SinusoidalPosInjection' --csvout SinusoidalPosInjection.csv --plotDeltaV
-python3 injectionCrashStats.py --infolder ../SinusoidalSpeedInjection/ --plotfolder graphs --plotname 'SinusoidalSpeedInjection' --csvout SinusoidalSpeedInjection.csv --plotDeltaV
-python3 injectionCrashStats.py --infolder ../SinusoidalAccInjection/ --plotfolder graphs --plotname 'SinusoidalAccInjection' --csvout SinusoidalAccInjection.csv --plotDeltaV
+#python3 crashStats.py --infolder raw_data/SinusoidalJammingDetail/ --plotfolder graphs --plotname 'SinusoidalJammingDetail' --csvout SinusoidalJammingDetail.csv --plotDeltaV
+python3 injectionCrashStats.py --infolder raw_data_new/SinusoidalPosInjection/ --plotfolder graphs_new --plotname 'SinusoidalPosInjection' --csvout SinusoidalPosInjection-new.csv --plotDeltaV
+python3 injectionCrashStats.py --infolder raw_data_new/SinusoidalSpeedInjection/ --plotfolder graphs_new --plotname 'SinusoidalSpeedInjection' --csvout SinusoidalSpeedInjection-new.csv --plotDeltaV
+python3 injectionCrashStats.py --infolder raw_data_new/SinusoidalAccInjection/ --plotfolder graphs_new --plotname 'SinusoidalAccInjection' --csvout SinusoidalAccInjection-new.csv --plotDeltaV
